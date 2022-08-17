7897abd364d711260544d3b0eaca55c1d0aeebeb
rodrigogomes1 <72028355+rodrigogomes1@users.noreply.github.com>
2022-08-17 11:25:32 +0100

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -17,8 +17,8 @@ def predict():
     inputEntry=[input]
     print(inputEntry)
     prediction = ml_algorithm.predict(inputEntry)
-    return jsonify({'prediction': list(prediction)})
-
+    #jsonify({'prediction': list(prediction)})
+    return "<h1>Prediction"+prediction+"!</h1>"
 
 if __name__ == '__main__': # If you don't provide any port then the port will be set to 12345
     ml_algorithm = joblib.load("model2.pkl") # Load "model.pkl"
