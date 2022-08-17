5c007f1c69d48b1c4722d44195e2b53f35b141b1
rodrigogomes1 <72028355+rodrigogomes1@users.noreply.github.com>
2022-08-17 11:08:25 +0100

    Add files via upload

---
+++ test_api.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+import joblib
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Welcome to machine learning mo2222del2 APIs-Waitress!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    json_ = request.json
+    input=[]
+    for key, value in json_[0].items():
+        input.append(value)
+    inputEntry=[input]
+    print(inputEntry)
+    prediction = ml_algorithm.predict(inputEntry)
+    return jsonify({'prediction': list(prediction)})
+
+
+if __name__ == '__main__': # If you don't provide any port then the port will be set to 12345
+    ml_algorithm = joblib.load("model2.pkl") # Load "model.pkl"
+    print ('Model loaded')
+    app.run(debug=True)
