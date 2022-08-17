e02eb2e5c391570ba05c73426ca636844428f66e
rodrigogomes1 <72028355+rodrigogomes1@users.noreply.github.com>
2022-08-17 11:39:49 +0100

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Welcome to our medium-greeting-api!</h1>"
+    return "<h1>Welcome to our Tool Recognition Machine Learning-API!</h1>"
 
 @app.route('/predict', methods=['POST'])
 def predict():
