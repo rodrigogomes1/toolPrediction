e74a07779faac2c66459c2bda91fc6f07278907f
rodrigogomes1 <72028355+rodrigogomes1@users.noreply.github.com>
2022-08-25 16:48:41 +0100

    Location Model Added

--- test_api.py
+++ test_api.py
@@ -6,11 +6,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Welcome to our Tool Recognition Machine Learning-API! By Ulisses!</h1>"
+    return "<h1>Welcome to our Tool/Location Recognition Machine Learning-API! By Ulisses!</h1>"
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    ml_algorithm = joblib.load("modelTrainedHome2.pkl")
+@app.route('/predict/tool', methods=['POST'])
+def predictTool():
+    ml_algorithm = joblib.load("model2.pkl")
+    json_ = request.json
+    input=[]
+    for key, value in json_[0].items():
+        input.append(value)
+    inputEntry=[input]
+    print(inputEntry)
+    prediction = ml_algorithm.predict(inputEntry)
+    #jsonify({'prediction': list(prediction)})
+    #"<h1>Prediction"+str(prediction)+"!</h1>",jsonify({'prediction': list(prediction)})
+    return str(prediction[0])
+
+@app.route('/predict/location', methods=['POST'])
+def predictLocation():
+    ml_algorithm = joblib.load("modelLocation.pkl")
     json_ = request.json
     input=[]
     for key, value in json_[0].items():
@@ -24,6 +38,6 @@ def predict():
 
 
 if __name__ == '__main__': # If you don't provide any port then the port will be set to 12345
-    ml_algorithm = joblib.load("modelTrainedHome2.pkl") # Load "model.pkl"
+    #ml_algorithm = joblib.load("modelTrainedHome2.pkl") # Load "model.pkl"
     print ('Model loaded')
     app.run(debug=True)
